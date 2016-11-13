1937587c897353b1ebc076451652301ee6439d39
Shawn Holmgren <VirusCentral@virus-central.attlocal.net>
2016-11-12 22:07:58 -0600

    add bookmark icon to button panel (non-working button)

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
-// change image of thumb down button on pandora
-// initial test for webpage alterations
+// working addition of bookmark button
 
-var thumbDown = document.getElementsByClassName("thumbDownButton");
-thumbDown.src = '/icon.png';
+var bookmarkButton = document.createElement('div');
+
+bookmarkButton.className ='bookmark';
+bookmarkButton.innerHTML = '<a href="#"><img src="https://cdn4.iconfinder.com/data/icons/bold-purple-free-samples/32/List_Text_Menu_Numbers_String_Burger-20.png"></a>';
+
+document.getElementById('playbackControl').appendChild(bookmarkButton);
+
+document.title = 'TEST';
+console.log("JavaScript is active.");
+
