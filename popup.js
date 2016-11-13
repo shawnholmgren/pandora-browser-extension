1e40953a168b7594a5a43af01a4691c174719351
Shawn Holmgren <VirusCentral@virus-central.attlocal.net>
2016-11-12 19:55:08 -0600

    add js to run on pandora page

---
+++ popup.js
@@ -0,0 +1,5 @@
+// change image of thumb down button on pandora
+// initial test for webpage alterations
+
+var thumbDown = document.getElementsByClassName("thumbDownButton");
+thumbDown.src = '/icon.png';
